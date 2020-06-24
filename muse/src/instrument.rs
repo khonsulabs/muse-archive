80452d62e5ed895253541b0f1c90091b161ee528
Jonathan Johnson <jon@khonsulabs.com>
2020-06-24 07:30:03 -0700

    Fixes #8. Reimplement sustain correctly.

--- muse/src/instrument.rs
+++ muse/src/instrument.rs
@@ -11,6 +11,7 @@ pub struct PlayingNote {
     pitch: u8,
     velocity: u8,
     sink: rodio::Sink,
+    key_is_pressed: bool,
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
@@ -36,6 +37,9 @@ impl VirtualInstrument {
         }
     }
     pub fn play_note(&mut self, pitch: u8, velocity: u8) -> Result<(), anyhow::Error> {
+        // We need to re-tone the note, so we'll get rid of the existing notes
+        self.playing_notes.retain(|n| n.pitch != pitch);
+
         let source = self.generate_note(pitch, velocity)?;
         let sink = rodio::Sink::new(&self.device);
         sink.append(source);
@@ -43,29 +47,33 @@ impl VirtualInstrument {
             pitch,
             velocity,
             sink,
+            key_is_pressed: true,
         });
 
         Ok(())
     }
 
     pub fn stop_note(&mut self, pitch: u8) {
-        if !self.sustain {
-            self.playing_notes.retain(|note| {
-                if note.pitch == pitch {
-                    note.sink.stop();
-                    false
-                } else {
-                    true
-                }
-            });
+        if self.sustain {
+            // For sustain, we need ot keep the notes playing, but mark that the key isn't pressed
+            // so that when the pedal is released, the note isn't filtered out.
+            if let Some(existing_note) = self
+                .playing_notes
+                .iter_mut()
+                .find(|note| note.pitch == pitch)
+            {
+                existing_note.key_is_pressed = false;
+            }
+        } else {
+            self.playing_notes.retain(|note| note.pitch != pitch);
         }
     }
 
     pub fn set_sustain(&mut self, active: bool) {
         self.sustain = active;
+
         if !active {
-            self.playing_notes.iter().for_each(|n| n.sink.stop());
-            self.playing_notes.clear();
+            self.playing_notes.retain(|n| n.key_is_pressed);
         }
     }
 
